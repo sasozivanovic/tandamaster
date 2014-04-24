a4a2119a7c3413efa6d8dd93af6cfb86f3392517
Sašo Živanović <saso.zivanovic@guest.arnes.si>
2014-04-24 05:26:32 +0200

    Playback works again.

--- ui.py
+++ ui.py
@@ -17,43 +17,63 @@ class TandaMasterWindow(QMainWindow):
         ptv = PlayTreeView('root', self.player)
         self.setCentralWidget(ptv)
         self.player.set_current(model = ptv.model(), silent = True)
-
-        self.playcontrols_actions = [
-            QAction(self.style().standardIcon(QStyle.SP_MediaSkipBackward), 
-                    self.tr('P&revious'), self, triggered = self.player.play_previous),
-            QAction(self.style().standardIcon(QStyle.SP_MediaPlay), 
-                    self.tr('&Play'), self, triggered = self.player.play),
-            QAction(self.style().standardIcon(QStyle.SP_MediaPause), 
-                    self.tr('&Pause'), self, triggered = self.player.pause),
-            QAction(self.style().standardIcon(QStyle.SP_MediaStop), 
-                    self.tr('&Stop'), self, triggered = self.player.stop),
-            QAction(self.style().standardIcon(QStyle.SP_MediaSkipForward), 
-                    self.tr('&Next'), self, triggered = self.player.play_next),
-        ]
+        
+        menubar = QMenuBar()
+        self.musicmenu = QMenu(self.tr('&Music'))
+        action_back = QAction(
+            self.style().standardIcon(QStyle.SP_MediaSkipBackward), 
+            self.tr('P&revious'), self, triggered = self.player.play_previous)
+        self.musicmenu.addAction(action_back)        
+        self.action_play = QAction(
+            self.style().standardIcon(QStyle.SP_MediaPlay), 
+            self.tr('&Play'), self, triggered = self.player.play)
+        self.musicmenu.addAction(self.action_play)        
+        self.action_pause =  QAction(
+            self.style().standardIcon(QStyle.SP_MediaPause), 
+            self.tr('&Pause'), self, triggered = self.player.pause)
+        self.musicmenu.addAction(self.action_pause)        
+        self.action_stop = QAction(
+            self.style().standardIcon(QStyle.SP_MediaStop), 
+            self.tr('&Stop'), self, triggered = self.player.stop)
+        self.musicmenu.addAction(self.action_stop)        
+        action_forward = QAction(
+            self.style().standardIcon(QStyle.SP_MediaSkipForward), 
+            self.tr('&Next'), self, triggered = self.player.play_next)
+        self.musicmenu.addAction(action_forward)        
+        self.musicmenu.addSeparator()
+        action_quit = QAction(
+            self.tr("&Quit"), self, shortcut=QKeySequence.Quit,
+            statusTip="Quit the program", triggered=self.close)
+        self.musicmenu.addAction(action_quit)
+        menubar.addMenu(self.musicmenu)
+        self.setMenuBar(menubar)
 
         toolbar = QToolBar('Play controls', self)
         toolbar.setAllowedAreas(Qt.TopToolBarArea | Qt.BottomToolBarArea)
         toolbar.setFloatable(False)
         toolbar.setIconSize(2*toolbar.iconSize())
-        for action in self.playcontrols_actions:
+        def create_playcontrol_button(action):
             button = QToolButton()
             button.setDefaultAction(action)
             button.setIconSize(2*button.iconSize())
             toolbar.addWidget(button)
+            return button
+        create_playcontrol_button(action_back)
+        self.button_play = create_playcontrol_button(self.action_play)
+        self.button_pause = create_playcontrol_button(self.action_pause)
+        create_playcontrol_button(self.action_stop)
+        create_playcontrol_button(action_forward)
         self.song_info = QLabel()
         toolbar.addWidget(QWidget())
         toolbar.addWidget(self.song_info)
         self.addToolBar(toolbar)
 
-        menubar = QMenuBar()
-        self.filemenu = QMenu(self.tr('&File'))
-        quitAction = QAction(self.tr("&Quit"), self, shortcut=QKeySequence.Quit,
-                                   statusTip="Quit the program", triggered=self.close)
-        self.filemenu.addAction(quitAction)
-        menubar.addMenu(self.filemenu)
-        self.setMenuBar(menubar)
-
         self.player.current_changed.connect(self.update_song_info)
+        self.player.stateChanged.connect(self.on_player_state_changed)
+        self.on_player_state_changed(QMediaPlayer.StoppedState)
+
+    def sizeHint(self):
+        return QSize(600, 800)
 
     def update_song_info(self, old_model, old_index, model, index):
         if index.isValid():
@@ -63,10 +83,24 @@ class TandaMasterWindow(QMainWindow):
         else:
             self.setWindowTitle("TandaMaster")
             self.song_info.setText("")
-            
 
-    def sizeHint(self):
-        return QSize(600, 800)
+    def on_player_state_changed(self, state):
+        if state == QMediaPlayer.PlayingState:
+            self.action_play.setEnabled(False)
+            self.action_pause.setEnabled(True)
+            self.action_stop.setEnabled(True)
+            self.button_play.setVisible(False) # todo!
+            self.button_pause.setVisible(True) # todo!
+        else:
+            self.action_play.setEnabled(True)
+            self.action_pause.setEnabled(False)
+            self.button_play.setVisible(True) # todo!
+            self.button_pause.setVisible(False) # todo!
+            if state == QMediaPlayer.StoppedState:
+                self.action_stop.setEnabled(False)
+            else:
+                self.action_stop.setEnabled(True)
+                
 
 class PlayTreeView(QTreeView):
 

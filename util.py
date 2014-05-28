44e567918586979bd003ba303e58fd7c03cdeb58
Sašo Živanović <saso.zivanovic@guest.arnes.si>
2014-05-28 17:14:57 +0200

    Add util.

---
+++ util.py
@@ -0,0 +1,28 @@
+# from http://stackoverflow.com/questions/20248355/how-to-get-python-to-gracefully-format-none-and-non-existing-fields
+import string
+class PartialFormatter(string.Formatter):
+    def __init__(self, missing='?', bad_fmt='!!'):
+        self.missing, self.bad_fmt=missing, bad_fmt
+
+    def get_field(self, field_name, args, kwargs):
+        # Handle a key not found
+        try:
+            val=super(PartialFormatter, self).get_field(field_name, args, kwargs)
+            # Python 3, 'super().get_field(field_name, args, kwargs)' works
+        except (KeyError, AttributeError):
+            val=None,field_name 
+        return val 
+
+    def format_field(self, value, spec):
+        # handle an invalid format
+        if value==None: return self.missing
+        try:
+            return super(PartialFormatter, self).format_field(value, spec)
+        except ValueError:
+            if self.bad_fmt is not None: return self.bad_fmt   
+            else: raise
+
+from PyQt5.Qt import QIcon
+def MyIcon(repository, category, name):
+    return QIcon('/usr/share/icons/{}/scalable/{}/{}'.format(repository, category, name))
+

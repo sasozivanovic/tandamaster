5d4df31d0c993adb5f5e3e4765c29597e23063ae
Sašo Živanović <saso.zivanovic@guest.arnes.si>
2015-02-26 23:22:28 +0100

    Add system tray icon.

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 import sys
-from PyQt5.Qt import QApplication, QUndoStack, pyqtSignal
+from PyQt5.Qt import QApplication, QUndoStack, pyqtSignal, QSystemTrayIcon, QIcon
 class TandaMasterApplication(QApplication):
     info = pyqtSignal(str)
 app = TandaMasterApplication(sys.argv)
+
+app.system_tray_icon = QSystemTrayIcon(QIcon('icons/iconarchive/icons8/tandamaster-Sports-Dancing-icon.png'))
+app.system_tray_icon.show()

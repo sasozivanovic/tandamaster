22aea61134ccd1063469092c9e7c32ca7b298f25
Sašo Živanović <saso.zivanovic@guest.arnes.si>
2016-08-04 03:11:24 +0200

    Create config/data paths if they don't exist.

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
-import sys
-from PyQt5.Qt import QApplication, pyqtSignal
+import sys, os
+from PyQt5.Qt import QApplication, pyqtSignal, QStandardPaths
 class TandaMasterApplication(QApplication):
     info = pyqtSignal(str)
 app = TandaMasterApplication(sys.argv)
 app.setApplicationName('TandaMaster')
 app.setOrganizationName('MilongueroSi')
 app.setOrganizationDomain('milonguero.si')
+try:
+    os.makedirs(os.path.normpath(QStandardPaths.writableLocation(QStandardPaths.AppDataLocation)))
+except FileExistsError:
+    pass
+try:
+    os.makedirs(os.path.normpath(QStandardPaths.writableLocation(QStandardPaths.AppConfigLocation)))
+except FileExistsError:
+    pass
